28cb802fae71876b98f93bb2e31dafd00baf6aab
cuervolu <cuervolu@protonmail.com>
2023-10-08 15:25:53 -0300

    fix(animation): Fix character not facing the same direction as last position
    - Resolved an issue where the character's animation was not correctly mirroring the last movement direction.
    
    refactor(collision): Make _castCollision readonly for now
    
    - Changed _castCollision to be a readonly list for improved safety and code clarity.
    
    style(formatting): Update code formatting
    
    - Improved the code formatting to enhance readability and maintainability.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,20 +6,20 @@ namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
-        [Header("Movement")]
-        
-        [SerializeField] private float speed = 8f;
+        [Header("Movement")] [SerializeField] private float speed = 8f;
         [SerializeField] private ContactFilter2D movementFilter;
         [SerializeField] private float collisionOffset = 0.05f;
-        private List<RaycastHit2D> _castCollision = new();
+        private readonly List<RaycastHit2D> _castCollision = new();
         private Vector2 _movementInput;
         private Vector2 _lastPosition = Vector2.zero;
-        
-        
+
+
         private Rigidbody2D _rb;
         private Animator _animator;
 
+
         #region Hashed Properties
+
         private static readonly int Speed = Animator.StringToHash("speed");
         private static readonly int Horizontal = Animator.StringToHash("horizontal");
         private static readonly int Vertical = Animator.StringToHash("vertical");
@@ -34,33 +34,25 @@ namespace Player
 
         private void FixedUpdate()
         {
-            if (_movementInput == Vector2.zero)
+            if (_movementInput != Vector2.zero)
+            {
+                var success = TryMove(_movementInput);
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(_movementInput.x, 0));
+
+                    if (!success)
+                        TryMove(new Vector2(0, _movementInput.y));
+                }
+
+                _animator.SetFloat(Speed, _movementInput.sqrMagnitude);
+            }
+            else
             {
                 _animator.SetFloat(Horizontal, _lastPosition.x);
                 _animator.SetFloat(Vertical, _lastPosition.y);
                 _animator.SetFloat(Speed, 0);
-                return;
-            }
-            var success = TryMove(_movementInput);
-
-            if (success) 
-            {
-                // Si el movimiento tuvo éxito, actualiza la última posición conocida.
-                _lastPosition = _rb.position;
-                return;
-            }
-
-            success = TryMove(new Vector2(_movementInput.x, 0));
-
-            if (!success)
-            {
-                success = TryMove(new Vector2(0, _movementInput.y));
-            }
-
-            if (success)
-            {
-                // Si el movimiento tuvo éxito, actualiza la última posición conocida.
-                _lastPosition = _rb.position;
             }
         }
 
@@ -69,7 +61,7 @@ namespace Player
 
         private bool TryMove(Vector2 direction)
         {
-            if(direction == Vector2.zero) return false;
+            if (direction == Vector2.zero) return false;
             var count = _rb.Cast(
                 direction,
                 movementFilter,
@@ -80,15 +72,14 @@ namespace Player
             _animator.SetFloat(Horizontal, _movementInput.x);
             _animator.SetFloat(Vertical, _movementInput.y);
             _animator.SetFloat(Speed, _movementInput.sqrMagnitude);
+            _lastPosition = direction;
             return true;
-
         }
 
         public void OnMove(InputValue movementValue)
         {
-           _movementInput = movementValue.Get<Vector2>();
+            _movementInput = movementValue.Get<Vector2>();
         }
-
 
         #endregion
     }
